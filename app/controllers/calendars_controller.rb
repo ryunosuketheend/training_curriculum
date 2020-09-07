ae3517592df0d465c870eacbda977af11fcb796e
ryunosuketheend <ryu.always.onemu@gmail.com>
2020-09-08 03:02:09 +0900

    merge用のcommit

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -13,30 +13,28 @@ class CalendarsController < ApplicationController
   end
 
   private
-
-  def plan_params
-    params.require(:calendars).permit(:date, :plan)
-  end
-
-  def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
-    @week_days = []
-
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
-
-    7.times do |x|
-      today_plans = []
-      plan = plans.map do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      @week_days.push(days)
+    def plan_params
+      params.require(:plan).permit(:date, :plan)
     end
 
+    def getWeek
+      wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+      # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+      @todays_date = Date.today
+      # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
+
+      @week_days = []
+
+      plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+      7.times do |x|
+        today_plans = []
+        plan = plans.map do |plan|
+          today_plans.push(plan.plan) if plan.date == @todays_date + x
+        end
+        days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+        @week_days.push(days)
+    end
   end
 end
